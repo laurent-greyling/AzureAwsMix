b12b89e561c3e9d057f554d8ed059b6cc1847e91
Laurent Greyling <Laurent.Greyling@rabobank.com>
2021-02-19 11:07:27 +0100

    add message to queue

---
+++ AzureFunctionCall/AzureMessageQueue.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionCall
+{
+    public class AzureMessageQueue
+    {
+        private static string _queueConnection = Environment.GetEnvironmentVariable("AzureWebJobsServiceBus");
+        private static string _queueName = "userdetails";
+
+        public static async Task Add(IEnumerable<Details> details)
+        {
+            var userDetails = JsonConvert.SerializeObject(details);
+            var queueClient = new QueueClient(_queueConnection, _queueName);
+
+            var message = new Message(Encoding.UTF8.GetBytes(userDetails));
+            message.UserProperties.Add("request", Guid.NewGuid());
+
+            await queueClient.SendAsync(message);
+        }
+    }
+}
--- AzureFunctionCall/CombineStringsFunction.cs
+++ AzureFunctionCall/CombineStringsFunction.cs
@@ -15,31 +15,30 @@ namespace AzureFunctionCall
             {
                 var details = DetermineDetails.Get(myQueueItem);
 
-                ////Write Read to Blob
-                //await AzureStorage.WriteBlob(details);
-                //var blobDetails = await AzureStorage.ReadBlob(details.Id);
+                //Write Read to Blob
+                await AzureStorage.WriteBlob(details);
+                var blobDetails = await AzureStorage.ReadBlob(details.Id);
 
-                ////Write Read to TableStorage
-                //details.PartitionKey = details.FirstName;
-                //details.RowKey = $"{details.Surname}-{details.Id}";
-                //await AzureStorage.WriteTable(details);
-                //var tableDetails = await AzureStorage.ReadTable(details.FirstName);
+                //Write Read to TableStorage
+                details.PartitionKey = details.FirstName;
+                details.RowKey = $"{details.Surname}-{details.Id}";
+                await AzureStorage.WriteTable(details);
+                var tableDetails = await AzureStorage.ReadTable(details.FirstName);
 
-                ////Write read to sql
-                //AzureSqlStorage.Save(details);
+                //Write read to sql
+                AzureSqlStorage.Save(details);
 
-                //await Task.Delay(5000);
+                await Task.Delay(5000);
 
                 var sqlDetails = AzureSqlStorage.Read<Details>(x => x.FirstName == details.FirstName); // && x.Surname == details.Surname && x.Id == details.Id && x.FullName == details.FullName);
 
+                await AzureMessageQueue.Add(sqlDetails);
                 log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");                
             }
             catch (Exception ex)
             {
-
                 throw;
-            }
-            
+            }            
         }
     }
 }
