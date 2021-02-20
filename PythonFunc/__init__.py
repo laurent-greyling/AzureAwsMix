dc0511510f1b48b1e00c4db0b2f28a4d3db944a5
Laurent Greyling <Laurent.Greyling@rabobank.com>
2021-02-20 09:39:02 +0100

    remove stuff

--- PythonFunc/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-import azure.functions as func
-from PythonFunc.create_details import CreateDetails
-
-def main(msg: func.ServiceBusMessage):
-    userDetails =  CreateDetails.get(msg)
-    logging.info('Python ServiceBus queue trigger processed message: %s',
-                 msg)
--- PythonFunc/details.py
+++
@@ -1,6 +0,0 @@
-class Details:
-    def __init__(self, Id, FirstName, Surname, FullName):
-        self.Id = Id
-        self.FirstName = FirstName
-        self.Surname = Surname
-        self.FullName = FullName
